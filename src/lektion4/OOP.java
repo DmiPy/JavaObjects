f9a50df5ba7446fa2dd028da82d08739bcddba7b
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2023-12-10 22:47:36 +0100

    first commit

---
+++ src/lektion4/OOP.java
@@ -0,0 +1,56 @@
+package lektion4;
+
+public class OOP {
+	public static class Bruch{
+		private static class Zahler {
+			private int zahler;
+			public Zahler(int x) {
+				zahler = x;
+			}
+			public int getZahler() {
+				return zahler;
+			}
+		}
+	
+		public static class Nenner{
+			private int nenner;
+			public Nenner(int y) {
+				nenner = y;
+			}
+			
+			public int getNenner() {
+				return nenner;
+			}
+		}
+
+
+		private Zahler numerator;
+		private Nenner denominator;
+		
+		public Bruch(int zahler, int nenner) {
+			this.numerator = new Zahler(zahler);
+			this.denominator = new Nenner(nenner);
+		}
+		
+		public Bruch(int ganzeZahl) {
+			this.numerator = new Zahler(ganzeZahl);
+			this.denominator = new Nenner(1);
+		}
+		
+		public int getZahler() {
+			return numerator.getZahler();
+		}
+		
+		public int getNenner() {
+			return denominator.getNenner();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Bruch fraction = new Bruch(2,3);
+		System.out.println(fraction.getZahler());
+
+	}
+
+}
