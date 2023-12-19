c33b18b329f60e6a67be70133ea802618ecc9ab8
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2023-12-19 20:23:57 +0100

    oop fractions implemented

--- src/lektion4/OOP.java
+++ src/lektion4/OOP.java
@@ -1,11 +1,6 @@
 package lektion4;
 
-import java.util.Scanner;
-
 public class OOP {
-	
-	public static Scanner input = new Scanner(System.in);
-	
 	
 	public static class Bruch{
 		private static class Zahler {
@@ -74,21 +69,75 @@ public class OOP {
 		static Bruch addiere(Bruch first, Bruch second) {
 			int numerator = 0;
 			int denominator = 0;
-			if (first.getNenner() == second.getNenner()) {
-				numerator = first.getZahler() + second.getZahler();
+			int first_num = first.getZahler();
+			int first_den = first.getNenner();
+			int sec_num = second.getZahler();
+			int sec_den = second.getNenner();
+			
+			if (first_den == sec_den) {
+				numerator = first_num + sec_num;
 				denominator = first.getNenner();
 				Bruch result = new Bruch(numerator,denominator);
 				return result;
 			}
 			
 			else {
-				int first_num = first.getZahler();
-				int first_den = first.getNenner();
-				int sec_num = second.getZahler();
-				int sec_den = second.getNenner();
-				System.out.println("НОК чисел " + first_den + " и " + sec_den + " = "+ lcm(first_den, sec_den));
-				return second;
+				denominator = lcm(first_den, sec_den);
+				int first_multiplicator = denominator / first_den;
+
+				first_num = first_multiplicator * first_num;
+				int second_multiplicator = denominator / sec_den;
+				sec_num = second_multiplicator * sec_num;
+				numerator = first_num + sec_num;
+				Bruch result = new Bruch(numerator,denominator);
+				return result;
 			}
+		}
+		
+		static Bruch substrahiere(Bruch first, Bruch second) {
+			int numerator = 0;
+			int denominator = 0;
+			int first_den = first.getNenner();
+			int sec_den = second.getNenner();
+			int first_num = first.getZahler();
+			int sec_num = second.getZahler();
+			if (first_den == sec_den) {
+				numerator = first_num - sec_num;
+				denominator = first.getNenner();
+				Bruch result = new Bruch(numerator,denominator);
+				return result;
+			}
+			
+			else {
+				denominator = lcm(first_den, sec_den);
+				int first_multiplicator = denominator / first_den;
+
+				first_num = first_multiplicator * first_num;
+				int second_multiplicator = denominator / sec_den;
+				sec_num = second_multiplicator * sec_num;
+				numerator = first_num - sec_num;
+				Bruch result = new Bruch(numerator,denominator);
+				
+				return result;
+			}
+		}
+		
+		static Bruch multipliziere(Bruch first, Bruch second) {
+			int first_den = first.getNenner();
+			int sec_den = second.getNenner();
+			int first_num = first.getZahler();
+			int sec_num = second.getZahler();
+			Bruch result = new Bruch(first_num * sec_num, first_den * sec_den);
+			return result;
+		}
+		
+		static Bruch dividiere(Bruch first, Bruch second) {
+			int first_den = first.getNenner();
+			int sec_den = second.getZahler();
+			int first_num = first.getZahler();
+			int sec_num = second.getNenner();
+			Bruch result = new Bruch(first_num * sec_num, first_den * sec_den);
+			return result;
 		}
 		
 		public String toString() {
@@ -99,20 +148,15 @@ public class OOP {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Geben Sie zwei Zahlen (Zahler) und (Nenner) ein!");
+		Bruch bruch1 = new Bruch(1,3);
+		Bruch bruch2 = new Bruch(4);
+		Bruch bruch3 = new Bruch(2,3);
 		
-		System.out.println("Die erste: ");
-		int x = input.nextInt();
+		Bruch result1 = Bruch.addiere(Bruch.substrahiere(bruch1,bruch2), bruch3);
+		Bruch result2 = Bruch.multipliziere(Bruch.dividiere(result1, bruch2),bruch3);
 		
-		System.out.println("Die zweite: ");
-		int y = input.nextInt();
-		
-		Bruch fraction = new Bruch(x,y);
-		Bruch fraction2 = new Bruch(5,7);
-//		System.out.println(fraction.toString());
-		
-		System.out.println(Bruch.addiere(fraction,fraction2));
-		
+		System.out.println(result1.toString());
+		System.out.println(result2.toString());
 	}
 
 }
